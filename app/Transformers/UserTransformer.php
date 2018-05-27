9873c3705bcfe243ae30b647e35edbe45cacdd07
vayupranaditya <bagusvayuprana@gmail.com>
2018-05-27 22:55:48 +0800

    payment proof upload and proposal upload finished

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -6,18 +6,24 @@ use App\User;
 
 use League\Fractal\TransformerAbstract;
 
+use Illuminate\Support\Facades\DB;
+
 class UserTransformer extends TransformerAbstract
 {
 	public function transform(User $user)
 	{
 		return [
-			'Team Name' 	=> $user->team_name,
-			'Institution'	=> $user->institution,
-			'Email'			=> $user->email,
-			'Team Member 1'	=> $user->team_member_1,
-			'Team Member 2'	=> $user->team_member_2,
-			'Team Member 3'	=> $user->team_member_3,
-			'Team Member 4'	=> $user->team_member_4,
+			'Team ID'				=> $user->id, 
+			'Team Name' 			=> $user->team_name,
+			'Institution'			=> $user->institution,
+			'Email'					=> $user->email,
+			'Team Member 1'			=> $user->team_member_1,
+			'Team Member 2'			=> $user->team_member_2,
+			'Team Member 3'			=> $user->team_member_3,
+			'Team Member 4'			=> $user->team_member_4,
+			'Active Student Proof'	=> asset('storage/active-student-proofs/' . DB::table('team_documents')
+				->where('team_id', $user->id)
+				->value('active_student_proof')),
 		];
 	}
 }
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,7 @@
 use Illuminate\Http\Request;
 
 Route::get('users', 'UserController@users');
+ROute::get('documents', 'DocumentController@documents');
+Route::post('document/payment-proof/{teamDocuments}', 'DocumentController@paymentProof')->middleware('auth:api');
+Route::post('document/proposal/{teamDocuments}', 'DocumentController@proposal')->middleware('auth:api');
 Route::post('auth/register', 'AuthController@register');
